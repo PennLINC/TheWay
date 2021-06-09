561a7f990207b36864f446c2ea1f91649be23517
Maxwell Bertolero <mbertolero@me.com>
2021-06-09 13:38:17 -0400

    Update bootstrap_matrices.sh

--- scripts/cubic/bootstrap_matrices.sh
+++ scripts/cubic/bootstrap_matrices.sh
@@ -226,9 +226,10 @@ datalad push --to output
 
 
 # Add an alias to the data in the RIA store
-# RIA_DIR=$(find $PROJECTROOT/output_ria/??? -maxdepth 1 -type d)
-# mkdir -p ${PROJECTROOT}/output_ria/alias
-# ln -s ${RIA_DIR} ${PROJECTROOT}/output_ria/alias/data
+RIA_DIR=$(find $PROJECTROOT/output_ria/??? -maxdepth 1 -type d)
+echo $RIA_DIR
+mkdir -p ${PROJECTROOT}/output_ria/alias
+ln -s ${RIA_DIR} ${PROJECTROOT}/output_ria/alias/data
 
 # if we get here, we are happy
 echo SUCCESS
