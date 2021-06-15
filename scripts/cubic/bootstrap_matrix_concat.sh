7c3b6b1798877f1237250d9f7450403588aaa5a3
Matt Cieslak <mattcieslak@gmail.com>
2021-06-15 11:48:03 -0400

    cleanup

--- scripts/cubic/bootstrap_matrix_concat.sh
+++ scripts/cubic/bootstrap_matrix_concat.sh
@@ -12,11 +12,13 @@ datalad clone ria+file://$DATA/matrices/output_ria#~data concat_ds
 wget https://raw.githubusercontent.com/PennLINC/RBC/master/PennLINC/Generic/matrix_concatenator.py
 
 datalad save -m "added matrix concatenator script"
-datalad run -i 'concat_ds/*matrices.zip*' -o 'group_matrices.zip' --expand inputs --explicit "python matrix_concatenator.py"
-
-# push changes
-datalad save -m "generated concatenated matrices"
-datalad push
+datalad run \
+    -m "generated concatenated matrices" \
+    -i 'concat_ds/*matrices.zip*' \
+    -o 'group_matrices.zip' \
+    --expand inputs \
+    --explicit \
+    "python matrix_concatenator.py"
 
 # remove concat_ds
 datalad uninstall concat_ds
