8f892631f1e9661f296175e67bf0712dc5a548ea
Maxwell Bertolero <mbertolero@me.com>
2021-08-11 14:21:59 -0400

    Update bootstrap-hcpya-xcp.sh

--- scripts/cubic/bootstrap-hcpya-xcp.sh
+++ scripts/cubic/bootstrap-hcpya-xcp.sh
@@ -36,8 +36,6 @@ then
     # exit 1
 fi
 
-module load connectome_workbench/1.4.2
-
 ##  hcp input
 HCPINPUT=https://github.com/datalad-datasets/human-connectome-project-openaccess
 if [[ -z ${HCPINPUT} ]]
@@ -143,9 +141,9 @@ dssource="$1"
 pushgitremote="$2"
 subid="$3"
 # change into the cluster-assigned temp directory. Not done by default in SGE
-# cd ${CBICA_TMPDIR}
+cd ${CBICA_TMPDIR}
 # OR Run it on a shared network drive
-cd /cbica/comp_space/$(basename $HOME)
+# cd /cbica/comp_space/$(basename $HOME)
 # Used for the branch names and the temp dir
 BRANCH="job-${JOB_ID}-${subid}"
 mkdir ${BRANCH}
