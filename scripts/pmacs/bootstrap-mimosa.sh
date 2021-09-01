53a775d6594f6aaae9766c6825640531ff5a9a12
Tim Robert-Fitzgerald <tim.terf@gmail.com>
2021-09-01 14:27:02 -0400

    wget on pmacs :(

--- scripts/pmacs/bootstrap-mimosa.sh
+++ scripts/pmacs/bootstrap-mimosa.sh
@@ -208,7 +208,6 @@ echo logs >> .gitignore
 datalad save -m "Participant compute job implementation"
 
 # Add a script for merging outputs
-MERGE_POSTSCRIPT=https://raw.githubusercontent.com/PennLINC/TheWay/main/scripts/cubic/merge_outputs_postscript.sh
 cat > code/merge_outputs.sh << "EOT"
 #!/bin/bash
 set -e -u -x
@@ -216,7 +215,71 @@ EOT
 echo "outputsource=${output_store}#$(datalad -f '{infos[dataset][id]}' wtf -S dataset)" \
     >> code/merge_outputs.sh
 echo "cd ${PROJECTROOT}" >> code/merge_outputs.sh
-wget -qO- ${MERGE_POSTSCRIPT} >> code/merge_outputs.sh
+cat >> code/merge_outputs.sh << "EOT"
+
+# The following should be pasted into the merge_outputs.sh script
+datalad clone ${outputsource} merge_ds
+cd merge_ds
+NBRANCHES=$(git branch -a | grep job- | sort | wc -l)
+echo "Found $NBRANCHES branches to merge"
+
+gitref=$(git show-ref master | cut -d ' ' -f1 | head -n 1)
+
+# query all branches for the most recent commit and check if it is identical.
+# Write all branch identifiers for jobs without outputs into a file.
+for i in $(git branch -a | grep job- | sort); do [ x"$(git show-ref $i \
+  | cut -d ' ' -f1)" = x"${gitref}" ] && \
+  echo $i; done | tee code/noresults.txt | wc -l
+
+
+for i in $(git branch -a | grep job- | sort); \
+  do [ x"$(git show-ref $i  \
+     | cut -d ' ' -f1)" != x"${gitref}" ] && \
+     echo $i; \
+done | tee code/has_results.txt
+
+mkdir -p code/merge_batches
+num_branches=$(wc -l < code/has_results.txt)
+CHUNKSIZE=5000
+set +e
+num_chunks=$(expr ${num_branches} / ${CHUNKSIZE})
+if [[ $num_chunks == 0 ]]; then
+    num_chunks=1
+fi
+set -e
+for chunknum in $(seq 1 $num_chunks)
+do
+    startnum=$(expr $(expr ${chunknum} - 1) \* ${CHUNKSIZE} + 1)
+    endnum=$(expr ${chunknum} \* ${CHUNKSIZE})
+    batch_file=code/merge_branches_$(printf %04d ${chunknum}).txt
+    [[ ${num_branches} -lt ${endnum} ]] && endnum=${num_branches}
+    branches=$(sed -n "${startnum},${endnum}p;$(expr ${endnum} + 1)q" code/has_results.txt)
+    echo ${branches} > ${batch_file}
+    git merge -m "merge results batch ${chunknum}/${num_chunks}" $(cat ${batch_file})
+
+done
+
+# Push the merge back
+git push
+
+# Get the file availability info
+git annex fsck --fast -f output-storage
+
+# This should not print anything
+MISSING=$(git annex find --not --in output-storage)
+
+if [[ ! -z "$MISSING" ]]
+then
+    echo Unable to find data for $MISSING
+    exit 1
+fi
+
+# stop tracking this branch
+git annex dead here
+
+datalad push --data nothing
+echo SUCCESS
+EOT
 
 
 ################################################################################
@@ -238,6 +301,11 @@ datalad save -m "LSF submission setup" code/ .gitignore
 # LSF SETUP END
 ################################################################################
 
+# make sure the fully configured output dataset is available from the designated
+# store for initial cloning and pushing the results.
+datalad push --to input
+datalad push --to output
+
 # cleanup - we have generated the job definitions, we do not need to keep a
 # massive input dataset around. Having it around wastes resources and makes many
 # git operations needlessly slow
@@ -245,11 +313,6 @@ if [ "${BIDS_INPUT_METHOD}" = "clone" ]
 then
     datalad uninstall -r --nocheck inputs/data
 fi
-
-# make sure the fully configured output dataset is available from the designated
-# store for initial cloning and pushing the results.
-datalad push --to input
-datalad push --to output
 
 # Add an alias to the data in the RIA store
 RIA_DIR=$(find $PROJECTROOT/output_ria/???/ -maxdepth 1 -type d | sort | tail -n 1)
