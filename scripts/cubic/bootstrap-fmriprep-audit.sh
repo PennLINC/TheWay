83f1153cb7a357de7a5aa9e73923ca92528da8b7
Sydney Covitz <70981267+scovitz@users.noreply.github.com>
2021-09-02 16:16:14 -0400

    Update bootstrap-fmriprep-audit.sh

--- scripts/cubic/bootstrap-fmriprep-audit.sh
+++ scripts/cubic/bootstrap-fmriprep-audit.sh
@@ -296,7 +296,7 @@ rm concatenator.*
 wget https://raw.githubusercontent.com/PennLINC/RBC/master/PennLINC/Generic/concatenator.py
 cd concat_ds
 datalad save -m "added concatenator script"
-datalad run -i 'csvs/*' -o '${tmpdir}/concat_ds/group_report.csv' --expand inputs --explicit "python code/concatenator.py ${tmpdir}/concat_ds/csvs ${PROJECT_ROOT}/PNC_FMRIPREP_AUDIT.csv"
+datalad run -i 'csvs/*' -o '${tmpdir}/concat_ds/group_report.csv' --expand inputs --explicit "python code/concatenator.py ${tmpdir}/concat_ds/csvs ${PROJECT_ROOT}/FMRIPREP_AUDIT.csv"
 
 datalad save -m "generated report"
 # push changes
