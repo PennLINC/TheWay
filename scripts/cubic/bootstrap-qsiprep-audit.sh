f948f7817e198a9a8e01bc9d4fb79c724eaec7eb
Sydney Covitz <70981267+scovitz@users.noreply.github.com>
2021-09-17 14:16:55 -0400

    Update bootstrap-qsiprep-audit.sh

--- scripts/cubic/bootstrap-qsiprep-audit.sh
+++ scripts/cubic/bootstrap-qsiprep-audit.sh
@@ -265,19 +265,23 @@ echo "cd ${PROJECTROOT}" >> code/concat_outputs.sh
 
 cat >> code/concat_outputs.sh << "EOT"
 # set up concat_ds and run concatenator on it
-cd ${CBICA_TMPDIR}
 datalad clone ria+file://${PROJECT_ROOT}/output_ria#~data concat_ds
 cd concat_ds/code
 wget https://raw.githubusercontent.com/PennLINC/RBC/master/PennLINC/Generic/concatenator.py
 cd ..
 datalad save -m "added concatenator script"
-datalad run -i 'csvs/*' -o '${tmpdir}/concat_ds/group_report.csv' --expand inputs --explicit "python code/concatenator.py ${tmpdir}/concat_ds/csvs ${PROJECT_ROOT}/FMRIPREP_AUDIT.csv"
+datalad run 
+    -i 'csvs/*' 
+    -o '${PROJECT_ROOT}/concat_ds/group_report.csv' 
+    --expand inputs
+    --explicit 
+    "python code/concatenator.py ${PROJECT_ROOT}/concat_ds/csvs ${PROJECT_ROOT}/QSIPREP_AUDIT.csv"
 datalad save -m "generated report"
 # push changes
 datalad push
 # remove concat_ds
 git annex dead here
-cd ${tmpdir}
+cd ${CBICA_TMPDIR}
 chmod +w -R concat_ds
 rm -rf concat_ds
 echo SUCCESS
