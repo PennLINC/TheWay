0730f990892601e186e3729a4d2dee24d55ed290
Sydney Covitz <70981267+scovitz@users.noreply.github.com>
2021-09-20 17:47:34 -0400

    Update bootstrap-qsiprep-audit.sh

--- scripts/cubic/bootstrap-qsiprep-audit.sh
+++ scripts/cubic/bootstrap-qsiprep-audit.sh
@@ -270,21 +270,17 @@ cd concat_ds/code
 wget https://raw.githubusercontent.com/PennLINC/RBC/master/PennLINC/Generic/concatenator.py
 cd ..
 datalad save -m "added concatenator script"
-datalad run 
-    -i 'csvs/*' 
-    -o '${PROJECT_ROOT}/concat_ds/group_report.csv' 
-    --expand inputs
-    --explicit 
-    "python code/concatenator.py ${PROJECT_ROOT}/concat_ds/csvs ${PROJECT_ROOT}/QSIPREP_AUDIT.csv"
+datalad run -i code/concatenator.py -i csvs/ -o ${PROJECT_ROOT}/concat_ds/group_report.csv --expand inputs --explicit "python code/concatenator.py ${PROJECT_ROOT}/concat_ds/csvs ${PROJECT_ROOT}/QSIPREP_AUDIT.csv"
 datalad save -m "generated report"
 # push changes
 datalad push
 # remove concat_ds
 git annex dead here
-cd ${CBICA_TMPDIR}
+cd ${PROJECT_ROOT}
 chmod +w -R concat_ds
 rm -rf concat_ds
 echo SUCCESS
+
 EOT
 
 #### concat_output.sh END ####
