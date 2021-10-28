665e34a7e15bd08de9ac8d94aa3a9bfd00edefa8
Sydney Covitz <70981267+scovitz@users.noreply.github.com>
2021-10-28 14:31:36 -0400

    Update bootstrap-quickunzip.sh

--- scripts/cubic/bootstrap-quickunzip.sh
+++ scripts/cubic/bootstrap-quickunzip.sh
@@ -2,7 +2,8 @@
 
 # used to extract all outputs from an xcp bootstrap dir 
 
-PROJECTROOT=/cbica/projects/RBC/production/PNC/xcp # make sure to change this to the root of your bootstrap dir!
+PROJECTROOT=/cbica/projects/hcpya/xcp # make sure to change this to the root of your bootstrap dir!
+
 cd ${HOME}
 mkdir DERIVATIVES
 cd ${HOME}/DERIVATIVES
@@ -10,7 +11,6 @@ RIA=${PROJECTROOT}/output_ria
 datalad create -c yoda -D "extract pnc xcp results" XCP
 cd XCP
 datalad clone -d . --reckless ephemeral "ria+file://${RIA}#~data" inputs/data
-
 
 ## the actual compute job specification
 cat > code/get_files.sh << "EOT"
@@ -22,7 +22,9 @@ ZIP_FILE=$1
 # Create a xcp/ directory
 unzip -n $ZIP_FILE
 
-outdir=xcp_abcd/$(basename $ZIP_FILE | sed 's/\.zip//')
+mv xcp_abcd/sub-* .
+
+rm -rf xcp_abcd
 
 EOT
 
@@ -31,20 +33,32 @@ datalad save -m "Add data extraction code" code
 zip_files=$(find inputs/data/ -name '*.zip')
 for input_zip in ${zip_files}
 do
-
-    outdir=xcp_abcd/$(basename $input_zip | sed 's/\.zip//')
-
+    subid=$(basename $input_zip | cut -d '_' -f 1)
+    html=${subid}.html
     datalad run \
         -i ${input_zip} \
-        -o ${outdir} \
+        -o ${subid} \
+        -o ${html} \
+        -m "unzipped ${input_zip}" \
         --explicit \
         "bash code/get_files.sh ${input_zip}"
 done
 
 echo 'DATALAD RUN FINISHED'
 
-# CRITICAL!!! Don't uninstall, just rm -rf the inputs
+# remove reckless ephemeral clone of zips
 rm -rf inputs
+
+# make inputs/data exist again so working directory is clean 
+mkdir -p inputs/data
+
+
+# FOR CONSUMERS OF DATA (if you want to see the inputs/data):
+# note that datalad get -n JUST gets the git history of the files 
+# datalad clone ~/RBC_DERIVATIVES/PNC/XCP test_xcp_outputs
+# datalad get -n inputs/data # to see xcp zips
+# datalad get -n inputs/data/inputs/data # to see fmriprep and freesurfer zips
+# datalad get -n inputs/data/inputs/data/inputs/data # to see BIDS!
 
 echo 'REMOVED INPUTS'
 echo 'SUCCESS'
