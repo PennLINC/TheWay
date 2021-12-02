6add9fb913f140b042d41e35798d77d473f13e63
Sydney Covitz <70981267+scovitz@users.noreply.github.com>
2021-12-02 15:50:06 -0500

    Update bootstrap-xcp-multises.sh

--- scripts/cubic/bootstrap-xcp-multises.sh
+++ scripts/cubic/bootstrap-xcp-multises.sh
@@ -257,10 +257,7 @@ datalad save -m "SGE submission setup" code/ .gitignore
 # cleanup - we have generated the job definitions, we do not need to keep a
 # massive input dataset around. Having it around wastes resources and makes many
 # git operations needlessly slow
-if [ "${BIDS_INPUT_METHOD}" = "clone" ]
-then
-    datalad uninstall -r --nocheck inputs/data
-fi
+datalad uninstall -r --nocheck inputs/data
 
 
 # make sure the fully configured output dataset is available from the designated
