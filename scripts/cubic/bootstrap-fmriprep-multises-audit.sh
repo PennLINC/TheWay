17e86f36acbfda64e008ac9b3047564f51bdfca0
Sydney Covitz <70981267+scovitz@users.noreply.github.com>
2022-01-07 11:38:30 -0500

    Update bootstrap-fmriprep-multises-audit.sh

--- scripts/cubic/bootstrap-fmriprep-multises-audit.sh
+++ scripts/cubic/bootstrap-fmriprep-multises-audit.sh
@@ -295,7 +295,7 @@ cd concat_ds/code
 wget https://raw.githubusercontent.com/PennLINC/RBC/master/PennLINC/Generic/concatenator.py
 cd ..
 datalad save -m "added concatenator script"
-datalad run -i 'csvs/*' -o 'concat_ds/group_report.csv' --expand inputs --explicit "python code/concatenator.py concat_ds/csvs ${PROJECT_ROOT}/FMRIPREP_AUDIT.csv"
+datalad run -i 'csvs/*' -o '${PROJECT_ROOT}/FMRIPREP_AUDIT.csv' --expand inputs --explicit "python code/concatenator.py csvs ${PROJECT_ROOT}/FMRIPREP_AUDIT.csv"
 datalad save -m "generated report"
 # push changes
 datalad push
@@ -304,7 +304,6 @@ git annex dead here
 cd ..
 chmod +w -R concat_ds
 rm -rf concat_ds
-
 echo SUCCESS
 
 EOT
