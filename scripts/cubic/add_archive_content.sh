29e61dd3128c58da192fbfd6362298c710e827b8
Sydney Covitz <70981267+scovitz@users.noreply.github.com>
2022-03-15 16:07:15 -0400

    Update add_archive_content.sh

--- scripts/cubic/add_archive_content.sh
+++ scripts/cubic/add_archive_content.sh
@@ -8,9 +8,11 @@
 source ${CONDA_PREFIX}/bin/activate base
 echo I\'m in $PWD using `which python`
 
+cd ${PWD}
+
 BOOTSTRAP_DIR=$1
 
-# clone output ria of  bootstrap dir 
+# clone output ria of bootstrap dir 
 datalad clone ria+file:///${BOOTSTRAP_DIR}/output_ria#~data archive_clone
 
 # cd into clone 
@@ -19,7 +21,7 @@ cd archive_clone
 for zip in *.zip; do
     echo Adding archive content for ${zip}
     datalad get ${zip}
-    datalad add-archive-content -e 'logs/.*' -e '.bidsignore' -e 'dataset_description.json' --drop-after ${zip}
+    datalad add-archive-content -e 'logs/.*' -e '.bidsignore' -e 'dataset_description.json' -e 'dwiqc.json' --drop-after ${zip}
 done
 
 datalad push
