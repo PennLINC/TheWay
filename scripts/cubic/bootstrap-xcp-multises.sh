5ceaf9c8417eb748be25d95920ed4848336b146b
Sydney Covitz <70981267+scovitz@users.noreply.github.com>
2022-07-22 09:57:14 -0400

    Update bootstrap-xcp-multises.sh

--- scripts/cubic/bootstrap-xcp-multises.sh
+++ scripts/cubic/bootstrap-xcp-multises.sh
@@ -205,6 +205,7 @@ cd inputs/data
 cd $wd
 
 mkdir -p ${PWD}/.git/tmp/wdir
+export SINGULARITYENV_TEMPLATEFLOW_HOME='~/.cache/templateflow'
 singularity run --cleanenv -B ${PWD} pennlinc-containers/.datalad/environments/xcp-abcd-0-0-4/image inputs/data/fmriprep xcp participant \
 --despike --lower-bpf 0.01 --upper-bpf 0.08 --participant_label $subid -p 36P -f 10 -w ${PWD}/.git/tmp/wkdir
 singularity run --cleanenv -B ${PWD} pennlinc-containers/.datalad/environments/xcp-abcd-0-0-4/image inputs/data/fmriprep xcp participant \
