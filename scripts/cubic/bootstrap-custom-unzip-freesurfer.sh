577f808b02c7f25e3c3357472e648ac3a85bd23e
Sydney Covitz <70981267+scovitz@users.noreply.github.com>
2022-11-16 14:09:42 -0500

    Update bootstrap-custom-unzip-freesurfer.sh

--- scripts/cubic/bootstrap-custom-unzip-freesurfer.sh
+++ scripts/cubic/bootstrap-custom-unzip-freesurfer.sh
@@ -18,7 +18,7 @@ set -e -u
 
 
 ## Set up the directory that will contain the necessary directories
-PROJECTROOT=${PWD}/FMRIPREP_UNZIPPED
+PROJECTROOT=${PWD}/FREESURFER_UNZIPPED
 if [[ -d ${PROJECTROOT} ]]
 then
     echo ${PROJECTROOT} already exists
